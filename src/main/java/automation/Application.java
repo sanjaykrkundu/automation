ce7540e3bc97bdbea19ed2298b62ae364e3aecce
sanjaykr.kundu <sanjaykr.kundu@gmail.com>
2018-09-04 21:50:00 +0530

    maven implemented

--- src/main/java/automation/Application.java
+++ src/main/java/automation/Application.java
@@ -6,10 +6,28 @@ import automation.framework.utils.YamlReader;
 public class Application {
 
 	public static void main(String[] args) {
+		
+		Configuration configuration = YamlReader.read("configuration.yaml", Configuration.class);
+		System.out.println(configuration);
 
-		//Configuration configuration = YamlReader.read("configuration.yaml",Configuration.class);
-		//System.out.println(configuration);
-			
+	/*	XmlSuite suite = new XmlSuite();
+		suite.setName("TmpSuite");
+
+		XmlTest test = new XmlTest(suite);
+		test.setName("TmpTest");
+		List<XmlClass> classes = new ArrayList<XmlClass>();
+		classes.add(new XmlClass("automation.irequest.sanity.HomePage"));
+		test.setXmlClasses(classes);
+
+		List<XmlSuite> suites = new ArrayList<XmlSuite>();
+		suites.add(suite);
+
+		System.out.println(suite);
+
+		TestNG tng = new TestNG();
+		tng.setXmlSuites(suites);
+		tng.run();*/
+
 	}
 
 }
--- src/main/java/automation/irequest/sanity/HomePage.java
+++ src/main/java/automation/irequest/sanity/HomePage.java
@@ -1,5 +1,6 @@
 package automation.irequest.sanity;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HomePage {
@@ -7,6 +8,7 @@ public class HomePage {
 	@Test
 	public void test(){
 		System.out.println("sanity.HomePage.test()");
+		Assert.assertFalse(true);
 	}
 	
 	
